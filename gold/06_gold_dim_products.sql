a38554c0cfff533c976360143ee8575d8e08b029
Manoel Alexandre Péres <a.peres777@gmail.com>
2026-03-24 10:04:14 -0300

    Add files via upload

--- gold/06_gold_dim_products.sql
+++ gold/06_gold_dim_products.sql
@@ -1,1 +1,13 @@
+-- Databricks notebook source
+INSERT INTO gold.dim_product (product_id)
 
+SELECT
+    s.product_id
+FROM silver.products s
+LEFT JOIN gold.dim_product d
+ON s.product_id = d.product_id
+WHERE d.product_id IS NULL;
+
+-- COMMAND ----------
+
+select * from gold.dim_product;
